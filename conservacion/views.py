ca77c24d01f3d71298428ff6de16c43b7f379f1e
Krazer001 <a.susanibar@pucp.edu.pe>
2022-05-24 20:22:13 -0500

    DataTable Agregado - Módulo Conservaciones

--- conservacion/views.py
+++ conservacion/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,7 +8,18 @@ from conservacion.models import ProyectoConservacion
 
 
 def Show(request):
-    context = {
-        'projects': ProyectoConservacion.objects.filter().all
-    }
-    return render(request,'proyectoConservacion/list.html',context)
+    if request.is_ajax():
+        print("Datatable")
+        data = ProyectoConservacion.objects.filter(estado='1').values('codigo','nombre','descripcion')
+        list_data = []
+        for indice,valor in enumerate(data):
+            list_data.append(valor)
+        print(list_data)
+        data = {
+            'length': data.count(),
+            'objects':list_data
+        }
+        return HttpResponse(json.dumps(data),'application/json')
+    else:
+        print("Render")
+        return render(request,'proyectoConservacion/list.html')
