e089dde9858e952abf1b4c6e180299623767c4ce
Kalli <kalli_28456@hotmail.com>
2022-05-25 21:17:31 -0500

    .

--- patrimonios/admin.py
+++ patrimonios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from patrimonios.models import Institucion, Patrimonio, Propietario
 
 # Register your models here.
+
+admin.site.register(Institucion)
+admin.site.register(Patrimonio)
+admin.site.register(Propietario)
