18a0bf9e283fb0461bcc252de1291dffe329e7a7
LuisG <luis.gaspar@ext.blaze.me>
2022-05-29 16:22:13 -0500

    Agregar vistas para gestión de incidentes - incidentes

--- incidente/urls.py
+++ incidente/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from incidente import views
 
 urlpatterns = [
-    
+    path('listar/', views.incidente_listar, name='incidente_listar'),
+    path('listar/detalle/', views.incidente_detalles, name='incidente_detalles'),
+
 ]
--- incidente/views.py
+++ incidente/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from incidente.models import Incidente
+from patrimonios.models import Patrimonio
+
+def incidente_listar(request):
+
+    context = {
+        'patrimonios': Patrimonio.objects.all()
+    }
+
+
+    return render(request, 'incidencia/patrimonio_incidente_listar.html', context=context)
+
+
+def incidente_detalles(request):
+
+    context = {
+        'incidentes': Incidente.objects.all()
+    }
+
+
+    return render(request, 'incidencia/incidente_listar.html', context=context)
