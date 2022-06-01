162edfc071b91c77603da8b3af0da2712bddd407
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2022-05-31 19:26:03 -0500

    Agregar front y conexion con back - Ver detalle patrimonio
    Insertado el boton que invoca al modal para ver el detalle de patrimonio (con ficha), el modal y la conexion del modal con back para mostrar un patrimonio (por defecto, Caral). Pendiente el formulario para elegir el patrimonio del cual se desea ver la ficha.

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
-from reportes import views
+from mapa import views
 
 urlpatterns = [
-
+    path('mapaficha/', views.mapaficha, name='mapaficha'),
 ]
--- mapa/views.py
+++ mapa/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
+from patrimonios.models import Servicio
+from patrimonios.models import Entrada
+from patrimonios.models import ActividadTuristica
+from patrimonios.models import Patrimonio
+from patrimonios.models import PatrimonioMaterial
+from patrimonios.models import PatrimonioMaterialMueble
+
 # Create your views here.
+
+def mapaficha(request):
+    patrimonies = Patrimonio.objects.filter(tituloDemoninacion='La Ciudad Sagrada Caral')
+    patrimony = patrimonies.first()
+    patrimonySchedules = PatrimonioMaterial.objects.filter(patrimonio__tituloDemoninacion='La Ciudad Sagrada Caral')
+    patrimonySchedule = patrimonySchedules.first()
+    activities = ActividadTuristica.objects.filter(actividadturisticaporpatrimonio__patrimonio__patrimonio_id=patrimony.id)
+    if len(activities) >1:
+        activities1 = activities[:len(activities) // 2]
+        activities2 = activities[len(activities) // 2:]
+        if len(activities2) > len(activities1):
+            activities2.reverse()
+            activities1.append(activities2.pop())
+            activities2.reverse()
+    else:
+        activities1 = activities
+        activities2 = []
+    tickets = Entrada.objects.filter(entradaporpatrimonio__patrimonio_id=patrimony.id)
+    services = Servicio.objects.filter(servicioporpatrimonio__patrimonio__patrimonio_id=patrimony.id)
+    if len(services) >1:
+        services1 = services[:len(services) // 2]
+        services2 = services[len(services) // 2:]
+        if len(services2) > len(services1):
+            services2.reverse()
+            services1.append(services2.pop())
+            services2.reverse()
+    else:
+        services1= services
+        services2 = []
+    context={
+        'patrimony':patrimony,
+        'patrimony_schedule':patrimonySchedule,
+        'activities1':activities1,
+        'activities2':activities2,
+        'tickets':tickets,
+        'services1':services1,
+        'services2': services2
+    }
+    return render(request, 'map/mapaFicha.html',context)
