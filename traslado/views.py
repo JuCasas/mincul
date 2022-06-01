35f33c0b8f3798b24b6330c9723cf4a8677cde3e
Juan <jcasasz@pucp.edu.pe>
2022-06-01 14:21:51 -0500

    Busqueda de patrimonios en agregarTraslado - traslado

--- traslado/views.py
+++ traslado/views.py
@@ -3,6 +3,7 @@ import datetime
 from django.shortcuts import render, redirect
 
 from authentication.models import User
+from patrimonios.models import Patrimonio
 from traslado.models import SolicitudTraslado, EntidadSolicitante
 
 from django.core import serializers
@@ -11,6 +12,7 @@ from django.http import JsonResponse
 def addTransfer(request):
     if request.POST:
         print(request.POST)
+        patrimoniosSolicitados = list(request.POST['lista'].split(","))
         solicitudTraslado = SolicitudTraslado.objects.create(entidadSolicitante_id=request.POST['nombreInstitucion'],
                                                              nombreExposicion=request.POST['nombreExposicion'],
                                                              pais=request.POST['pais'],
@@ -23,17 +25,30 @@ def addTransfer(request):
                                                                  'fechaRetornoProgramada'],
                                                              numeroResolucion=request.POST['nResolucion']
                                                              )
+        for idPatrimonio in patrimoniosSolicitados:
+            solicitudTraslado.patrimonios.add(Patrimonio.objects.get(pk=idPatrimonio))
+
         return redirect('list_transfers')
     else:
         entidades = EntidadSolicitante.objects.filter()
         comisarios = User.objects.filter(groups__name="Gestor de Conservacion y Traslados")
+        patrimonios = Patrimonio.objects.filter()
 
         context = {
             'entidades': entidades,
-            'comisarios': comisarios
+            'comisarios': comisarios,
+            'patrimonios': patrimonios,
         }
         return render(request, 'traslado/transfer_add.html', context)
 
+def listarPatrimoniosTraslado(request):
+    filtro = request.GET['q']
+    print("//////////////////////////////////////////////////////////")
+    patrimonios = Patrimonio.objects.filter()
+    ser_instance = serializers.serialize('json', list(patrimonios),
+                                         fields=('id', 'tituloDemoninacion','categoria', 'tipoPatrimonio'))
+    print(ser_instance)
+    return JsonResponse({"patrimoniosAjax": ser_instance}, status=200)
 
 def listTranfers(request):
 
