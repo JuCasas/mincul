49b01aafb0f9f941de14654db44be1e8cacce7d1
Diego <dlozano099@gmail.com>
2022-06-15 01:33:42 -0500

    Avance Editar patrimonios - patrimonios

--- patrimonios/urls.py
+++ patrimonios/urls.py
@@ -5,4 +5,6 @@ from patrimonios import views
 urlpatterns = [
     path('listar/', views.patrimonio_list, name='patrimonio_list'),
     path('edit/<pk>', views.patrimonio_edit, name='patrimonio_edit'),
+    path('listarInstituciones/',views.instituciones_list_api, name='instituciones_list_api'),
+    path('listarGestorPatrimonio/',views.gestorPatrimonio_list_api, name='gestorPatrimonio_list_api'),
 ]
