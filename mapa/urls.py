78d492f11057baecf3d4b2240069c8578836c4bb
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2022-06-14 23:57:21 -0500

    Update urls.py

--- mapa/urls.py
+++ mapa/urls.py
@@ -4,5 +4,6 @@ from mapa import views
 
 urlpatterns = [
     path('ficha/<my_id>/',views.ficha,name='ficha'),
-    path('mapa',views.mapa,name='mapa'),
+    path('buscarpatrimoniosimple',views.mapaPatrimonioSimple,name='mapaPatrimonioSimple'),
+    path('buscarpatrimonioavanzado',views.mapaPatrimonioAvanzado,name='mapaPatrimonioAvanzado'),
 ]
