87901134649ef4f0aecde1fdb33106be8c82b8d2
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2022-06-15 00:00:39 -0500

    Update views.py

--- mapa/views.py
+++ mapa/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from patrimonios.models import Patrimonio
 
-
 # Create your views here.
 
 def ficha(request,my_id):
@@ -11,6 +10,26 @@ def ficha(request,my_id):
     }
     return render(request,'map/ficha.html',context)
 
-def mapa(request):
+def mapaPatrimonioSimple(request):
+    if request.POST:
+        patrimonioNombre = request.POST['patrimonio_name']
+        patrimonio = None
+        print(":::")
+        print(patrimonioNombre)
+        print(":::")
+        if patrimonioNombre != '' or patrimonioNombre != None:
+            patrimonio = Patrimonio.objects.get(nombreTituloDemoninacion=patrimonioNombre)
+            return render(request, 'map/mapaBusquedaSimplePatrimonio.html', {'patrimonio': patrimonio})
+    return render(request, 'map/mapaBusquedaSimplePatrimonio.html', {})
 
-    return render(request,'map/mapaBase.html')
+def mapaPatrimonioAvanzado(request):
+    if request.POST:
+        patrimonioNombre = request.POST['patrimonio_name']
+        patrimonio = None
+        print(":::")
+        print(patrimonioNombre)
+        print(":::")
+        if patrimonioNombre != '' or patrimonioNombre != None:
+            patrimonio = Patrimonio.objects.get(nombreTituloDemoninacion=patrimonioNombre)
+            return render(request, 'map/mapaBusquedaAvzPatrimonio.html', {'patrimonio': patrimonio})
+    return render(request, 'map/mapaBusquedaAvzPatrimonio.html', {})
