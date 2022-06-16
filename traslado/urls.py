a5dee4b3d6cead1d8833dfbdf620f5a299f03f95
gilmerwilder00 <71391608+gilmerwilder00@users.noreply.github.com>
2022-06-16 00:42:13 -0500

    Fix dropzone  en agregar solicitudTraslado - Traslados

--- traslado/urls.py
+++ traslado/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from traslado import views
 
 urlpatterns = [
-    #path('',views.listProjects, name='listTransfer'),
-    # path('add/',views.addTransfer, name='addTransfer'),
+
     path('agregar/',views.addTransfer, name='addTransfer'),
     path('listar/',views.listTranfers, name='list_transfers'),
     path('ver/<int:pk>/', views.viewTranfer, name='view_transfer'),
