336659623cc6e0ea853a693bc4cbc4554c78a0ca
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2022-06-16 17:44:32 -0500

    Cambios busqueda patrimonios

--- mapa/urls.py
+++ mapa/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('ficha/<my_id>/',views.ficha,name='ficha'),
     path('buscarpatrimoniosimple',views.mapaPatrimonioSimple,name='mapaPatrimonioSimple'),
     path('buscarpatrimonioavanzado',views.mapaPatrimonioAvanzado,name='mapaPatrimonioAvanzado'),
+    path('punto/<pk>', views.mapaPunto, name='mapaPunto')
 ]
--- mapa/views.py
+++ mapa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from patrimonios.models import Patrimonio
-
+from django.http import JsonResponse
 # Create your views here.
 
 def ficha(request,my_id):
@@ -13,23 +13,99 @@ def ficha(request,my_id):
 def mapaPatrimonioSimple(request):
     if request.POST:
         patrimonioNombre = request.POST['patrimonio_name']
-        patrimonio = None
-        print(":::")
-        print(patrimonioNombre)
-        print(":::")
-        if patrimonioNombre != '' or patrimonioNombre != None:
-            patrimonio = Patrimonio.objects.get(nombreTituloDemoninacion=patrimonioNombre)
-            return render(request, 'map/mapaBusquedaSimplePatrimonio.html', {'patrimonio': patrimonio})
-    return render(request, 'map/mapaBusquedaSimplePatrimonio.html', {})
+        patrimonioNombre.rstrip()
+        patrimonioNombre.lstrip()
+        if patrimonioNombre != '' or patrimonioNombre != None or patrimonioNombre != ' '\
+                or patrimonioNombre != "" or patrimonioNombre != " ":
+            patrimonios = Patrimonio.objects.filter(nombreTituloDemoninacion__icontains=patrimonioNombre)
+            if len(patrimonios) > 0:
+                patrimonio0 = patrimonios[0]
+                context = {
+                    'patrimonios': patrimonios,
+                    'patrimonio0': patrimonio0
+                }
+                return render(request, 'map/mapaBusquedaSimplePatrimonio.html', context)
+    return render(request, 'map/mapaBusquedaSimplePatrimonio.html')
 
 def mapaPatrimonioAvanzado(request):
     if request.POST:
         patrimonioNombre = request.POST['patrimonio_name']
-        patrimonio = None
+        patrimonioCategoria = request.POST['select_categoria']
+        patrimoniodepartamento = request.POST['select_departamento']
+        patrimonioprovincia = request.POST['select_provincia']
+        patrimoniodistrito = request.POST['select_distrito']
+        patrimonioNombre.rstrip()
+        patrimonioNombre.lstrip()
         print(":::")
         print(patrimonioNombre)
+        print(patrimonioCategoria)
+        print(patrimoniodepartamento)
+        print(patrimonioprovincia)
+        print(patrimoniodistrito)
         print(":::")
-        if patrimonioNombre != '' or patrimonioNombre != None:
-            patrimonio = Patrimonio.objects.get(nombreTituloDemoninacion=patrimonioNombre)
-            return render(request, 'map/mapaBusquedaAvzPatrimonio.html', {'patrimonio': patrimonio})
-    return render(request, 'map/mapaBusquedaAvzPatrimonio.html', {})
+        if patrimonioNombre != '' or patrimonioNombre != None or patrimonioNombre != ' '\
+            or patrimonioNombre != "" or patrimonioNombre != " ":
+            patrimonios = Patrimonio.objects.filter(nombreTituloDemoninacion__icontains=patrimonioNombre)
+            patriNom=[]
+            patriDep=[]
+            patriProv=[]
+            patriDis=[]
+            if len(patrimonios) > 0:
+                for p in patrimonios:
+                    if p.categoria == 1 and patrimonioCategoria =='Mueble':
+                        patriNom.append(p)
+                    elif p.categoria == 2 and patrimonioCategoria=='Inmueble':
+                        patriNom.append(p)
+                    elif patrimonioCategoria=='Categoría':
+                        patriNom.append(p)
+                if len(patriNom) >0:
+                    for p in patriNom:
+                        if p.departamento == patrimoniodepartamento:
+                            patriDep.append(p)
+                        elif patrimoniodepartamento=='Departamento':
+                            patriDep.append(p)
+                    if len(patriDep)>0:
+                        for p in patriDep:
+                            if p.provincia == patrimonioprovincia:
+                                patriProv.append(p)
+                            elif patrimonioprovincia == 'Provincia':
+                                patriProv.append(p)
+                        if len(patriProv) > 0:
+                            for p in patriProv:
+                                if p.provincia == patrimonioprovincia:
+                                    patriDis.append(p)
+                                elif patrimonioprovincia == 'Provincia':
+                                    patriDis.append(p)
+                            if len(patriDis) > 0:
+                                patriFinal=[]
+                                if patrimoniodistrito!="Distrito":
+                                    for p in patriDis:
+                                        if p.distrito == patrimoniodistrito:
+                                            patriFinal.append(p)
+                                    if len(patriFinal)>0:
+                                        patrimonio0 = patriFinal[0]
+                                        context = {
+                                            'patrimonios': patriFinal,
+                                            'patrimonio0': patrimonio0
+                                        }
+                                        return render(request, 'map/mapaBusquedaAvzPatrimonio.html', context)
+                                else:
+                                    patrimonio0 = patriDis[0]
+                                    context = {
+                                        'patrimonios': patriDis,
+                                        'patrimonio0': patrimonio0
+                                    }
+                                    return render(request, 'map/mapaBusquedaAvzPatrimonio.html',context)
+    return render(request, 'map/mapaBusquedaAvzPatrimonio.html')
+
+def mapaPunto(request,pk):
+    patrimonio=Patrimonio.objects.get(id=pk)
+    context = {
+        'patrimony':{
+            'nombreTituloDenominacion':patrimonio.nombreTituloDemoninacion,
+            'lat':patrimonio.lat,
+            'long':patrimonio.long
+        }
+    }
+    print(context)
+    return JsonResponse(context, status=200)
