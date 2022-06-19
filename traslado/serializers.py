481afacd1a933b52603c73733f6b48b5970be2ba
Juan <jcasasz@pucp.edu.pe>
2022-06-19 05:09:15 -0500

    Fix comisario username a full name - traslado

--- traslado/serializers.py
+++ traslado/serializers.py
@@ -7,7 +7,11 @@ class TrasladoSerializer(serializers.ModelSerializer):
     fechaSalidaProgramada = serializers.DateField(format='%d/%m/%Y', required=False)
     fechaRetornoProgramada = serializers.DateField(format='%d/%m/%Y', required=False)
     entidadSolicitante = serializers.CharField(source='entidadSolicitante.nombreSolicitante')
-    gestorConservacionTraslados = serializers.CharField(source='gestorConservacionTraslados.username')
+    gestorConservacionTraslados = serializers.SerializerMethodField()
+
+    def get_gestorConservacionTraslados(self, obj):
+        return '{} {}'.format(obj.gestorConservacionTraslados.first_name, obj.gestorConservacionTraslados.last_name)
+
     class Meta:
         model = SolicitudTraslado
         fields = ['id','entidadSolicitante','gestorConservacionTraslados','fechaRetornoProgramada','fechaSalidaProgramada','estado']
