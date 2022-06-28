9f9975238128f40d1b93200d35fa594bfe67e893
Diego <dlozano099@gmail.com>
2022-06-28 17:07:07 -0500

    Avance Detalle patrimonio

--- patrimonios/serializers.py
+++ patrimonios/serializers.py
@@ -38,3 +38,27 @@ class PatrimonioListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patrimonio
         fields = ['id','nombreTituloDemoninacion','tipo','categoria','imagen']
+
+class PatrimonioSerializer(serializers.ModelSerializer):
+
+    imagen = serializers.SerializerMethodField('getImg')
+    categoria = serializers.SerializerMethodField('getCategory')
+    tipo = serializers.SerializerMethodField('getTipo')
+
+    def getImg(self, obj):
+        doc = Documento.objects.filter(patrimonio__id=obj.pk).first()
+        if doc != None:
+            return '/'+str(doc.url)
+        return 'static/img/imageNotAvailable.jpg'
+
+    def getCategory(self, obj):
+        cat = Categoria.objects.get(pk=obj.categoria_id).nombre
+        return cat
+
+    def getTipo(self, obj):
+        tipo = Patrimonio._meta.get_field('tipoPatrimonio').choices[int(obj.tipoPatrimonio) - 1]
+        return tipo
+
+    class Meta:
+        model = Patrimonio
+        fields = ['id','nombreTituloDemoninacion','descripcion','observacion','direccion','departamento','provincia','distrito','tipo','categoria','imagen']
