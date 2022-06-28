7ae45db147670ea43687b794a165690214855173
Miguel Zenteno <mizero97@hotmail.com>
2022-06-28 02:32:06 -0500

    fix visualizar incidente - incidentes
    fix visualizar incidente - incidentes

--- incidente/serializers.py
+++ incidente/serializers.py
@@ -1,18 +1,29 @@
+import os
 from cgitb import reset
 
 from rest_framework import serializers
 from incidente.models import Incidente
+from mincul_app.models import Documento
 from patrimonios.models import PuntoGeografico
+
+class DocumentoSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    class Meta:
+        model = Documento
+        fields = ['id','name','url']
+    def get_name(self, instance):
+        return os.path.basename(instance.url.name)
 
 
 class IncidenteSerializer(serializers.ModelSerializer):
     fechaRegistro = serializers.DateField(format='%d/%m/%Y', required=False)
     zona = serializers.SerializerMethodField()
     proyecto = serializers.SerializerMethodField()
+    documentos = DocumentoSerializer(read_only=True, many=True)
 
     class Meta:
         model = Incidente
-        fields = ['id','codigo','nombre','correo','telefono','descripcion','zona','tipoAfectacion','fechaRegistro','status','proyecto']
+        fields = ['documentos','id','codigo','nombre','correo','telefono','descripcion','zona','tipoAfectacion','fechaRegistro','status','proyecto']
 
     def get_zona(self, obj):
         return obj.zona.nombre
